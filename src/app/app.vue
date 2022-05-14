9a495830296a43719c06aa91952045eea2a6b381
lilei <xuanyu200910@163.com>
2022-05-14 17:07:39 +0800

    监听事件 v-on

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,6 +1,8 @@
 <template>
-<h3>{{ name }}</h3>
-{{processStatus}}
+  <h3>{{ name }}</h3>
+  {{processStatus}}
+  <button v-on:click="resetName">重置</button>
+  <button @click="setName">恢复</button>
 </template>
 
 
@@ -19,6 +21,13 @@ computed:{
     return this.name ==='Ninghao'? '初始化...' : '成功设置了数据';
   }
 },
+
+watch:{
+  name(newName,oldName){
+    console.log(`name 发生了变化: ${oldName}->${newName}`);
+
+  }
+},
   created(){
     console.log('APP 组件已创建');
     this.setName();
@@ -31,6 +40,15 @@ computed:{
            this.name = '宁皓⽹';
          },3000);
     },
+    resetName(){
+      this.name='Ninghao';
+    }
   },
 };
 </script>
+
+<style>
+  button{
+    margin:4px;
+  }
+</style>
