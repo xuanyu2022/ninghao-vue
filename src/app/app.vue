6d79076c2a1d5d7b86be71a7895c1f38e3a9fa02
lilei <xuanyu200910@163.com>
2022-05-14 22:02:34 +0800

    条件判断 v-if

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,8 +1,7 @@
 <template>
   <h3>{{ name }}</h3>
-  <div v-for="(post,index)  in postList" :key="post.id">
-     {{index + 1}} {{ post.content }} - <small>{{ post.author }}</small>
-  </div>
+ <div v-if="visible">隐藏的内容!</div>
+  <button @click="visible =!visible">{{visible? '隐藏':'显示'}} </button>
 </template>
 
 
@@ -12,23 +11,7 @@
       data() {
         return {
           name:'Ninghao',
-          postList: [
-             {
-                id: 1,
-                content: '故⼈西辞黄鹤楼，烟花三⽉下扬州',
-                author: '李⽩',
-              },
-              {
-                id: 2,
-                content: '好⾬知时节，当春乃发⽣',
-                author: '杜甫',
-              },
-              {
-                id: 3,
-                content: '浔阳江头夜送客，枫叶荻花秋瑟瑟',
-                author: '⽩居易',
-              },
-          ],
+          visible:true,
         };
       },
     };
