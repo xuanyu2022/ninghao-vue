0c185e88d10039fb4a9c5f5dc7bbcc96b030739f
lilei <xuanyu200910@163.com>
2022-05-15 10:38:55 +0800

    绑定class属性

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,7 +1,18 @@
 <template>
   <h3>{{ name }}</h3>
- <div v-if="visible">隐藏的内容!</div>
-  <button @click="visible =!visible">{{visible? '隐藏':'显示'}} </button>
+ 
+ <!--<div v-if="visible">隐藏的内容!</div>
+  <button @click="visible =!visible">{{visible? '隐藏':'显示'}} </button>-->
+
+ <div class="menu">
+    <div 
+        :class="['menu-item',{active:currentItem===index}]"
+        @click="currentItem=index"
+        v-for="(item,index) in menuItems" :key="index"
+    >
+    {{item}}
+    </div>
+</div>
 </template>
 
 
@@ -12,13 +23,24 @@
         return {
           name:'Ninghao',
           visible:true,
+          menuItems:['首页','热门','发布'],
+          currentItem:0,
         };
       },
     };
 </script>
 
+
+
 <style>
-  button{
-    margin:4px;
-  }
+ .menu{
+   display: flex;
+   gap:16px;  
+}
+ .active{
+   color:#6435c9;
+   font-weight:bold;
+ }
+
+
 </style>
