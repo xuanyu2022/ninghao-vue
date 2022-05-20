ad3d52e7e6f9e11372e3547ba32966264b26843d
lilei <xuanyu200910@163.com>
2022-05-20 18:22:13 +0800

    路由链接, 切换地址,

---
+++ src/app/components/about.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>宁皓⽹创建于 2011 年。</div>
+</template>
---
+++ src/app/components/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div @click="onClickText">你好，欢迎访问宁皓⽹！</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClickText() {
+      this.$router.push('/about');
+      // this.$router.replace('/about');
+    },
+  },
+};
+</script>
