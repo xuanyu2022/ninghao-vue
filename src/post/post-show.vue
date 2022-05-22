eee1c0ac09b08cbd09b2fb92471c54ca38de8916
lilei <xuanyu200910@163.com>
2022-05-22 09:05:06 +0800

    路由组件里的导航卫士

--- src/post/post-show.vue
+++ src/post/post-show.vue
@@ -8,6 +8,11 @@ export default {
   props: {
     postId: String,
   },
+  beforeRouteLeave(to, from, next) {
+    if (confirm('确定要离开吗？ ')) {
+      next();
+    }
+  },
 
   created() {
     console.log(this.$route.params.postId);
