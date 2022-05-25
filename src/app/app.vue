c24ed88e9a472d6d8196ae68bde9850132fab4c6
lilei <xuanyu200910@163.com>
2022-05-25 15:33:10 +0800

    9.3动画-(过渡)

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,24 +1,31 @@
 <template>
-  <h3>{{ name }}</h3>
- 
-
+  <div class="page">
+    <div class="card">
+      <div class="card__header">
+        <h3 class="card-title">{{ name }}</h3>
+        <div class="card-subtitle">Transition & Animation</div>
+      </div>
+      <div class="card__content">
+        <!--  <div :class="['emoji', { hidden: !isActive }]">🌲</div> -->
+        <div :class="['emoji', { pulse: isActive }]">😍</div>
+      </div>
+      <div class="card__action">
+        <button @click="isActive = !isActive">请按这⾥</button>
+      </div>
+    </div>
+  </div>
 </template>
-
-
-
 <script>
-    export default {
-      data() {
-        return {
-          name:'Ninghao',
-         
-        };
-      },
+export default {
+  data() {
+    return {
+      name: '宁皓⽹',
+      isActive: true,
     };
+  },
+};
 </script>
-
-
-
 <style>
 @import './styles/app.css';
+@import './styles/card.css';
 </style>
