eb2c0d984835a140b8e67962f0799a6f28797a70
lilei <xuanyu200910@163.com>
2022-05-28 08:33:28 +0800

    理解vue的反应系统

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,23 +1,50 @@
 <template>
-  <h3>{{ name }}</h3>
- 
-
+  <h3 @click="name = 'NINGHAO'">{{ name }}</h3>
 </template>
 
-
-
 <script>
-    export default {
-      data() {
-        return {
-          name:'Ninghao',
-         
-        };
+export default {
+  data() {
+    return {
+      name: 'Ninghao',
+    };
+  },
+  created() {
+    console.log(this.$data);
+  },
+
+  /* created() {
+    const user = {
+      name: '宁皓⽹',
+    };
+  },
+  const handler = {
+       // get() {
+        //return 'NINGHAO'; }
+      get(target, property) {
+        return target[property];
+      },
+      set(target, property, value) {
+        if (property === 'name') {
+          if (value.length > 10) {
+            throw new Error('名字太长了！ ');
+          }
+        }
+        return Reflect.set(target, property, value);
       },
     };
+
+    const userProxy = new Proxy(user, handler);
+
+    try {
+      userProxy.name = 'NINGHAO1';
+    } catch (error) {
+      console.log('错误： ', error.message);
+    }
+    console.log(userProxy.name);
+  },*/
+};
 </script>
-
-
 
 <style>
 @import './styles/app.css';
