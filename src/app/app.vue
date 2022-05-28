0a4d68e1cf3ec5a4ab5abf9937c7304da37130c0
lilei <xuanyu200910@163.com>
2022-05-28 11:03:27 +0800

    提供组件的方法, methods平替

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,16 +1,27 @@
 <template>
   <h3 @click="name = 'NINGHAO'">{{ name }}</h3>
+  <div @click="user.name = 'WANGHAO'">{{ user.name }}</div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 export default {
   setup() {
-    const name = ref('宁皓⽹');
+    const name = ref('宁皓网');
+    const user = reactive({
+      name: '王皓',
+    });
+    const changeName = () => {
+      name.value = 'NINGHAO';
+    };
     console.log(name.value);
+    console.log(user);
+
     return {
       name,
+      user,
+      changeName,
     };
   },
 };
