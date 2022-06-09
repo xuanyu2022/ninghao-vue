0046c2da0604329d4e3e2af1d49d2afa24a0ae38
lilei <xuanyu200910@163.com>
2022-06-10 07:13:18 +0800

    安装和使用http客户端请求服务端接口axios

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,23 +1,28 @@
 <template>
   <h3>{{ name }}</h3>
- 
-
+  <div v-for="post in posts" :key="post.id">
+    {{ post.title }} -
+    <small>{{ post.user.name }}</small>
+  </div>
 </template>
 
-
-
 <script>
-    export default {
-      data() {
-        return {
-          name:'Ninghao',
-         
-        };
-      },
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      name: '宁皓⽹',
+      posts: [],
     };
+  },
+  created() {
+    axios.get('http://localhost:3000/posts').then(response => {
+      console.log(response);
+      this.posts = response.data;
+    });
+  },
+};
 </script>
-
-
 
 <style>
 @import './styles/app.css';
