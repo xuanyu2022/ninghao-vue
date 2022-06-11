22f5fcdf8ded35a8668215737d86fccee7e34bde
lilei <xuanyu200910@163.com>
2022-06-11 12:14:27 +0800

    发送post类型的http请求_补充

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,9 +1,11 @@
 <template>
   <h3>{{ name }}</h3>
+
   <div v-for="post in posts" :key="post.id">
     {{ post.title }} -
     <small>{{ post.user.name }}</small>
   </div>
+  <input type="text" v-model="title" @keyup.enter="createPost" />
   <div>{{ errorMessage }}</div>
 </template>
 
@@ -21,18 +23,42 @@ export default {
         password: '123123',
       },
       token: '',
+      title: '',
     };
   },
   async created() {
+    try {
+      const response = await apiHttpClient.get('/posts');
+
+      this.posts = response.data;
+    } catch (error) {
+      this.errorMessage = error.message;
+    }
+
     try {
       const response = await apiHttpClient.post('/login', this.user);
       // console.log(axios.defaults);
       this.token = response.data.token;
       console.log(response.data);
-      console.log(this.token);
+      //console.log(this.token);
     } catch (error) {
       this.errorMessage = error.message;
     }
+  },
+
+  methods: {
+    async createPost() {
+      try {
+        const response = await apiHttpClient.post('/posts', {
+          title: this.title,
+        });
+        console.log(response.data);
+        this.title = '';
+        this.getPosts();
+      } catch (error) {
+        this.errorMessage = error.message;
+      }
+    },
   },
 };
 </script>
