30961915933d978596d15ac7d801436623cebd1c
lilei <xuanyu200910@163.com>
2022-07-26 12:31:12 +0800

    创建与使⽤ Vuex 的 Store

---
+++ src/app/app.store.ts
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex';
+/**
+* 创建 Store
+*/
+const store = createStore({
+state: {
+appName: '宁皓⽹',
+},
+});
+/**
+* 默认导出
+*/
+export default store;
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { createApp } from 'vue';
 import App from './app/app.vue';
-
+import appStore from './app/app.store';
 
 /**
 * 创建应⽤
 */
 const app = createApp(App);
+
+/**
+* 应⽤ Store
+*/
+app.use(appStore);
+
 /**
 * 挂载应⽤
 */
