114d58d8eadbc9088929fdaa64148fd8b057c0e1
lilei <xuanyu200910@163.com>
2022-08-06 17:43:35 +0800

    14.6.1 vue应用的环境变量配置

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,15 @@
 import axios from 'axios';
-
+import { API_BASE_URL } from './app.config';
+/**
+ * HTTP 客户端
+ */
 export const apiHttpClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: API_BASE_URL,
 });
+
+// import axios from 'axios';
+// import { API_BASE_URL } from './app.config';
+
+// export const apiHttpClient = axios.create({
+//   baseURL: 'http://localhost:3000',
+// });
