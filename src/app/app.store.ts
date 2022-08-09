0828ee94f3669305796357ddc4583e8baee6b671
lilei <xuanyu200910@163.com>
2022-08-09 14:34:29 +0800

    14.7.4  定义   内容页面store模块

--- src/app/app.store.ts
+++ src/app/app.store.ts
@@ -1,11 +1,10 @@
 import { createStore } from 'vuex';
 import { PostCreateStoreState } from '../post/create/post-create.store';
-import { AbcdEfg, postStoreModule, PostStoreState } from '../post/post.store';
+import { postStoreModule, PostStoreState } from '../post/post.store';
 
 export interface RootState {
   appName: string;
   post: PostStoreState;
-  name: AbcdEfg;
   name2: PostCreateStoreState;
 }
 /**
